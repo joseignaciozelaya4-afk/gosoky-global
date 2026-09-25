b2d354d1accfebda3e8e57824811393e24251035
joseignaciozelaya4-afk <joseignaciozelaya4@gmail.com>
2026-09-25 10:25:40 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
-
-const root = fileURLToPath(new URL('.', import.meta.url));
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': root,
-      '@/components': root,
-      '@/services': root,
-      '@/contexts': root,
-      '@/lib': root,
+      '@': path.resolve(__dirname, '.'),
     },
   },
 });
